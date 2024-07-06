b83cf7f44617bf610c8e7bc4ae8be226b48c57ab
leticea <nascimento.leticeah@gmail.com>
2024-07-06 13:10:13 -0300

    feat(register-for-event.ts): creating register route

--- src/routes/create-event.ts
+++ src/routes/create-event.ts
@@ -1,8 +1,8 @@
+import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
-import { generateSlug } from "../utils/generate-slug";
 import { prisma } from "../lib/prisma";
-import { FastifyInstance } from "fastify";
+import { generateSlug } from "../utils/generate-slug";
 
 export async function createEvent(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -33,7 +33,7 @@ export async function createEvent(app: FastifyInstance) {
       });
 
       if (eventWithSameSlug !== null) {
-        throw new Error("Another event with same title already exists.");
+        throw new Error("Another event with the same title already exists.");
       }
 
       const event = await prisma.event.create({
