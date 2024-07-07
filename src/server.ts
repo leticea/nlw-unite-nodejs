be7ecb9d023ae5640fec97802609485d64fba9f1
leticea <nascimento.leticeah@gmail.com>
2024-07-07 15:59:10 -0300

    add fastify/cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import fastify from "fastify";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
 import {
   serializerCompiler,
   validatorCompiler,
   jsonSchemaTransform,
+  ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
@@ -13,8 +15,13 @@ import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
+import { errorHandler } from "./error-handler";
 
-export const app = fastify();
+export const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.register(fastifySwagger, {
   swagger: {
@@ -44,6 +51,8 @@ app.register(getAttendeeBadge);
 app.register(checkIn);
 app.register(getEventAttendees);
 
-app.listen({ port: 3333 }).then(() => {
+app.setErrorHandler(errorHandler);
+
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running!");
 });
